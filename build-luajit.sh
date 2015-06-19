8fd12c69c173eb377b028304c837db22eaaa366e
Developer <>
2015-06-19 12:32:39 -0430

    add option to pull the LuaJIT submodule from the build script

--- build-luajit.sh
+++ build-luajit.sh
@@ -2,14 +2,14 @@
 ################################################################################
 #                                 IMPORTANT
 ################################################################################
-#  This script is intended to be called from the build-phase in Xcode to build
-#  LuaJIT as a staic library.
+#  This script is intended to be called from the build-phase in Xcode in order
+#  to build LuaJIT as a static library.
 #
-#  Pass only those environment variables that are used in the script, otherwise
+#  Pass only those environment variables that are used by the script, otherwise
 #  the build process may fail.
 #
 #  To use it just copy and paste the following in a run script phase either for
-#  iOS, OS X, or both, and uncomment the line.
+#  iOS, OS X, or both, and uncomment the lines.
 #
 #   env -i \
 #		ISDK="$SDKROOT" \
@@ -28,26 +28,29 @@
 
 shopt -s extglob
 
+# Uncomment this to pull LuaJIT submodule, this will take a while the first run.
+#git submodule update --init luajit
+
 LUAJIT=luajit/src
 
-# Get architectures contained in the previously build fat library
+# Get architectures contained in the previously build fat library.
 LIPO_ARCHS=""
 
 if [ -e $BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME ]; then
     LIPO_ARCHS=`lipo -info $BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME | sed -e 's,^Archi.* are: ,,' -e 's,^Non-fat.*ture: ,,'`
 fi
 
-# Create the products dir
+# Create the products dir.
 mkdir -p $BUILT_PRODUCTS_DIR
 
-# Build a static library for each architecture in the list of targeted ones
+# Build a static library for each architecture in the list of targeted ones.
 for ARCH in $ARCHS ; do
-    # Skip build if the architecture is already in the fat binary
+    # Skip build if the architecture is already in the fat binary.
 	if [[ $LIPO_ARCHS =~ (^| )$ARCH($| ) ]]; then
 		continue
 	fi
 
-    # Build the library
+    # Build the library.
 	HOST_ARCH="-m32 -arch i386"
 
 	if [ "$ARCH" == "arm64" ] || [ "$ARCH" == "x86_64" ] ; then
@@ -77,7 +80,7 @@ for ARCH in $ARCHS ; do
 		exit 1
 	fi
 
-    # Add the library to the fat binary
+    # Add the library to the fat binary.
 	mv "$LUAJIT/libluajit.a" $BUILT_PRODUCTS_DIR/libluajit-$ARCH.a
 
     if [ -e $BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME ]; then
@@ -86,6 +89,6 @@ for ARCH in $ARCHS ; do
         lipo -create $BUILT_PRODUCTS_DIR/libluajit-$ARCH.a -output $BUILT_PRODUCTS_DIR/$EXECUTABLE_NAME
     fi
 
-    # Remove all specific architecture libraries keeping only the fat version
+    # Remove all specific architecture libraries keeping only the fat version.
     rm $BUILT_PRODUCTS_DIR/!($EXECUTABLE_PREFIX$PRODUCT_NAME).a
 done
